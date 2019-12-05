5ddc48508d65d69f788092646257facb1e017d9a
sophiatan0101 <57200563+sophiatan0101@users.noreply.github.com>
2019-12-05 16:00:09 -0500

    Update models.py

--- squirrel/sightings/models.py
+++ squirrel/sightings/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+class squirrel(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    uniqueid = models.CharField(max_length=150)
+    shift = models.CharField(max_length=150)
+    Date = models.IntegerField()
+    Age = models.CharField(max_length=150)
+    furcolor = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
+    specificlocation = models.CharField(max_length=150)
+    running = models.CharField(max_length=150)
+    chasing = models.CharField(max_length=150) 
+    climbing = models.CharField(max_length=150)
+    eating = models.CharField(max_length=150)
+    foraging = models.CharField(max_length=150)
+    otheractivities = models.CharField(max_length=150)
+    kuks = models.CharField(max_length=150)
+    quaas = models.CharField(max_length=150)
+    moans = models.CharField(max_length=150)
+    tailflags = models.CharField(max_length=150)
+    tailtwitches = models.CharField(max_length=150)
+    approaches = models.CharField(max_length=150)
+    indifferent = models.CharField(max_length=150)
+    runsfrom = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
 # Create your models here.
